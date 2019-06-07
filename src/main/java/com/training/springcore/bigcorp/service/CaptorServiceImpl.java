d6311d3213eade41165d6f7b75ecbb078b36445c
Yengo <jerzy.yengo@gmail.com>
2019-06-07 15:18:33 +0200

    tp9_passageASpringBoot

---
+++ src/main/java/com/training/springcore/bigcorp/service/CaptorServiceImpl.java
@@ -0,0 +1,26 @@
+package com.training.springcore.bigcorp.service;
+
+import com.training.springcore.bigcorp.model.Captor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class CaptorServiceImpl implements CaptorService{
+    @Value("${bigcorp.measure.default-simulated}")
+    private Integer defaultValue;
+
+
+    @Override
+    @Monitored
+    public Set<Captor> findBySite(String siteId) {
+        Set<Captor> captors = new HashSet<>();
+        if (siteId == null) {
+            return captors;
+        }
+        captors.add(new Captor("Capteur A"));
+        return captors;
+    }
+}
